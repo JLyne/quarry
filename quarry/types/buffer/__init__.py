0e424beebcd6b8a5c049a7fbeac7e2f596348cf5
James Lyne <jim+github@not-null.co.uk>
2024-12-03 18:18:50 +0000

    1.21.4 buffer updates

--- quarry/types/buffer/__init__.py
+++ quarry/types/buffer/__init__.py
@@ -6,6 +6,7 @@ from quarry.types.buffer.v1_20_3 import Buffer1_20_3
 from quarry.types.buffer.v1_20_5 import Buffer1_20_5
 from quarry.types.buffer.v1_21 import Buffer1_21
 from quarry.types.buffer.v1_21_2 import Buffer1_21_2
+from quarry.types.buffer.v1_21_4 import Buffer1_21_4
 
 
 # Versioned buffers used after handshaking
@@ -14,6 +15,7 @@ buff_types = [
     (766, Buffer1_20_5),
     (767, Buffer1_21),
     (768, Buffer1_21_2),
+    (769, Buffer1_21_4),
 ]
 
 
---
+++ quarry/types/buffer/item/v1_21_4.py
@@ -0,0 +1,68 @@
+from typing import TYPE_CHECKING
+
+from quarry.types.buffer.item.v1_21_2 import ItemBuffer1_21_2
+
+if TYPE_CHECKING:
+    from quarry.types.buffer import Buffer1_21_2
+
+
+class ItemBuffer1_21_4(ItemBuffer1_21_2):
+    component_handlers = ItemBuffer1_21_2.component_handlers
+    component_handlers['custom_model_data'] = lambda cls: cls.pack_custom_model_data, lambda self: self.unpack_custom_model_data,
+
+    def __init__(self, buffer: 'Buffer1_21_2'):
+        super(ItemBuffer1_21_2, self).__init__(buffer)
+
+    # Equippable ---------------------------------------------------------------
+
+    @classmethod
+    def pack_equippable(cls, value):
+        value['asset_id'] = value.get('model', None) # Model renamed to asset_id
+        del value['model']
+
+        return super().pack_equippable(value)
+
+    def unpack_equippable(self):
+        data = super().unpack_equippable()
+
+        data['asset_id'] = data.get('Model', None) # Model renamed to asset_id
+        del data['model']
+
+        return data
+
+    # Custom Model Data ---------------------------------------------------------------
+
+    @classmethod
+    def pack_custom_model_data(cls, value):
+        print('???')
+        floats = value.get('floats', [])
+        flags = value.get('flags', [])
+        strings = value.get('strings', [])
+        colors = value.get('colors', [])
+
+        data = cls.buffer.pack_varint(len(floats)) + \
+            cls.buffer.pack_array('f', floats) + \
+            cls.buffer.pack_varint(len(flags)) + \
+            cls.buffer.pack_array('?', flags) + \
+            cls.buffer.pack_varint(len(strings))
+
+        for string in strings:
+            data += cls.buffer.pack_string(string)
+
+        data += cls.buffer.pack_varint(len(colors))
+
+        for color in colors:
+            if isinstance(color, list): # int rgb array
+                data += cls.buffer.pack('i', color[0]<<16 + color[1]<<8 + color[2])
+            else: # Single int
+                data += cls.buffer.pack('i', color)
+
+        return data
+
+    def unpack_custom_model_data(self):
+        return {
+            'floats': self.buffer.unpack_array('f', self.buffer.unpack_varint()),
+            'flags': self.buffer.unpack_array('?', self.buffer.unpack_varint()),
+            'strings': [self.buffer.unpack_string() for _ in self.buffer.unpack_varint()],
+            'colors': [self.buffer.unpack('i') for _ in self.buffer.unpack_varint()],
+        }
