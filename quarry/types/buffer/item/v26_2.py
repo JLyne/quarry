89ebbbaade08a8f8f0c7387edadafdfca382692d
James Lyne <jim+github@not-null.co.uk>
2026-09-11 16:20:59 +0100

    Fix sulfur_cube_content component position

--- quarry/types/buffer/item/v26_2.py
+++ quarry/types/buffer/item/v26_2.py
@@ -6,8 +6,10 @@ if TYPE_CHECKING:
     from quarry.types.buffer import Buffer26_2
 
 class ItemBuffer26_2(ItemBuffer26_1):
-    component_handlers = dict(ItemBuffer26_1.component_handlers)
-    component_handlers['sulfur_cube_content'] = (lambda cls: cls.pack_item, lambda self: self.unpack_item)
+    component_handlers = list(ItemBuffer26_1.component_handlers.items())
+    component_handlers.insert(78, ('sulfur_cube_content', (lambda cls: cls.pack_item, lambda self: self.unpack_item)))
+
+    component_handlers = dict(component_handlers)
     component_types = list(component_handlers.keys())
 
     def __init__(self, buffer: 'Buffer26_2'):
